f8d1434544d464892921c26e06f58a3cfcbea533
/配置姓名 <1299898126@qq.com>
2020-12-10 08:47:21 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant 核心组件库
+import Vant from 'vant'
+
+// 加载vant 全局样式
+import 'vant/lib/index.css'
+
+// 加载全局样式
+import './styles/index.less'
+
+// 加载动态设置 rem 基准值
+import 'amfe-flexible'
+
+// 注册使用 vant 组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: (h) => h(App),
 }).$mount('#app')
