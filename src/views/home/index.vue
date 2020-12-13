8ab30b731ecd098e71fbac734e1f6c6e194d5a88
/配置姓名 <1299898126@qq.com>
2020-12-13 11:25:33 +0800

    06

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,46 @@
-<!--
- * @Author: your name
- * @Date: 2020-12-12 15:17:43
- * @LastEditTime: 2020-12-12 15:18:53
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \toutiao-m\src\views\home\index.vue
--->
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button class="search-btn"
+                  slot="title"
+                  type="info"
+                  size="small"
+                  round
+                  icon="search">搜索</van-button>
+    </van-nav-bar>
+    <!-- 频道列表 -->
+    <van-tabs class="channel-tabs"
+              v-model="active"
+              animated
+              swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+
+      <div slot="nav-right"
+           class="placeholder"></div>
+      <div slot="nav-right"
+           class="hamburger-btn">
+        <i class="toutiao toutiao-gengduo"></i>
+      </div>
+    </van-tabs>
+  </div>
 </template>
+
 
 <script>
 export default {
-  name: 'HomePage',
+  name: 'HomeIndex',
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      active: 0,
+    }
   },
   computed: {},
   watch: {},
@@ -26,4 +50,74 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+// 当前组件中加了 scoped 对内部样式的修改需要加 /deep/，或者去掉 scoped
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+  /deep/ .channel-tabs {
+    .van-tabs__wrap {
+      height: 82px;
+    }
+    // Tab 标签页
+    .van-tab {
+      border-right: 1px solid #edeff3;
+      min-width: 200px;
+      font-size: 30px;
+      color: #777777;
+    }
+    .van-tab--active {
+      color: #333333;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0;
+    }
+    .van-tabs__line {
+      bottom: 8px;
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
+    }
+    // 汉堡
+    .placeholder {
+      flex-shrink: 0;
+      width: 66px;
+      height: 82px;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 66px;
+      height: 82px;
+      background-color: #fff;
+      background-color: rgba(255, 255, 255, 0.902);
+      i.iconfont {
+        font-size: 33px;
+      }
+      &:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 58px;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
+    }
+  }
+}
+</style>
